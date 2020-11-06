4fffc795e8a9bfeeb3cfc535a51d27caefb56bca
PeachDo <a39550897@gmail.com>
2020-11-06 19:40:24 +0900

    main-doyeon

--- web/src/pages/main/Main.tsx
+++ web/src/pages/main/Main.tsx
@@ -1,61 +1,126 @@
-import React from 'react'
-import MainBoard from '../../organisms/main/MainBoard';
+import React from 'react';
+import { fade, createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import { Grid,Button } from '@material-ui/core';
-import Appbar from '../../organisms/appbar/Appbar';
+import Grid from '@material-ui/core/Grid';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+import Appbar from '../organisms/Appbar';
+import Navi from '../organisms/Navi';
+import logo_2 from '../logo_2.png';
 
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
 
-export default function Main(): JSX.Element{
-const [loadingMode, setLoading] = React.useState(true);
+    paper: {
+      padding: theme.spacing(5),
+      textAlign: 'center',
+    },
+    
 
-function handleLoadingModeOn(){
-  setLoading(true);
-}
-function handleLoadingModeOff() {
-  setLoading(false);
-}
-  return(
- 
-  <div style={{width: '80%', margin: '20px auto'}}>
-    <Button 
-      style={{marginLeft: 20, marginRight: 5}} 
-      variant="contained" 
-      color="primary" 
-      onClick={handleLoadingModeOff}
-      > skeletonON 
-    </Button>
-    <Button
-      style={{marginLeft: 10, marginRight: 5}}  
-      variant="contained" 
-      color="secondary" 
-      onClick={handleLoadingModeOn}
-      > skeletonOff
-    </Button>
-  <div style={{padding: 20}}>
-   
-   <Appbar/>
-   <Paper variant="outlined" >
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+      },
+    },
 
-    <Grid container xs={12} spacing={2}>
-     <Grid item xs={4} >
-        <MainBoard handleLoadingModeON={handleLoadingModeOn} data={loadingMode}/>
-      </Grid>
+      searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+
+      inputRoot: {
+        color: 'inherit',
+      },
+
+      inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+          width: '20ch',
+        },
+      },
+
+  }),
+);
+
+
+export default function _main() {
+  const classes = useStyles();
   
-      <Grid item xs={4} >
-        <MainBoard handleLoadingModeON={handleLoadingModeOn} data={loadingMode}/>
-      </Grid>
+  return(
+  <div style={{width: '100%', margin: 0}}>
+    <Appbar/>
+  <div style={{width:'100%', margin: '50px'}}>
+    {/* <Navi/> */}
 
-      <Grid item xs={4}>
-        <MainBoard handleLoadingModeON={handleLoadingModeOn} data={loadingMode}/>
-      </Grid>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          </Paper>
+        </Grid>
 
-      <Grid item xs={4}>
-        <MainBoard handleLoadingModeON={handleLoadingModeOn} data={loadingMode}/>
-      </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+          </Paper>
+        </Grid>
 
-    </Grid>
-    </Paper>
+        <Grid item xs={6} sm={2}>
+          <Paper className={classes.paper}>
+          <Link to='/rooms'><img src={logo_2}/></Link>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={2}>
+          <Paper className={classes.paper}>
+          <Link to='/health'><img src={logo_2}/></Link>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={2}>
+          <Paper className={classes.paper}>
+          <Link to='/books'><img src={logo_2}/></Link>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
-  </div>);
+  </div>
+  </div>
+
+  );
 }
